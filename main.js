9bd66ec2d941ddc16b2588e7c081e043633d110d
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2020-07-07 11:26:06 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ let ctx = cnv.getContext("2d");
 cnv.width = 800;
 cnv.height = 600;
 
+let array = [500, 200, 350, 400, 580, 170, 225, 325];
+
 // Main Program Loop
 requestAnimationFrame(draw);
 
@@ -14,6 +16,15 @@ function draw() {
 
     // Drawing
     ctx.clearRect(0, 0, cnv.width, cnv.height);
+
+    // draw bar graph
+    ctx.fillStyle = 'orange';
+    ctx.strokeStyle = 'grey';
+    for (let i = 0; i < array.length; i++) {
+        ctx.fillRect(i*100, cnv.height - array[i], cnv.width/array.length, array[i]);
+        ctx.strokeRect(i*100, cnv.height - array[i], cnv.width/array.length, array[i]);
+    }
+
 
     // Request another Animation Frame
     requestAnimationFrame(draw);
